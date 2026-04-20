d72b089bd2536f7b50591adf081b47f6255a25fc
Keenan Clough <149612324+amounderness@users.noreply.github.com>
2026-04-20 14:42:59 +0100

    Update SITE_TITLE to new domain name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Keenan Clough";
+export const SITE_TITLE = "keenanclough.ai";
 export const SITE_DESCRIPTION = "Human Ambition x Autonomous Systems";
